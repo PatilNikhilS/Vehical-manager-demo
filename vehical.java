66c9da414b3fdded62fdaa86dba580b8fb8e0353
Nikhilpatil <nikhilpatil5252@gmail.com>
2023-07-26 10:49:59 +0530

    disel Engine class added

--- vehical.java
+++ vehical.java
@@ -2,7 +2,9 @@ public class vehical {
 
     public static void main(String[] args) {
         PetroleEngine pE = new PetroleEngine();
+        pE.start();
 
+        DiselEngine dE = new DiselEngine();
         pE.start();
     }
 }
