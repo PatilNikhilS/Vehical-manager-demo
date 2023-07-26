8875d1b15ae81fc56f2545799709e3530eb551ff
Nikhilpatil <nikhilpatil5252@gmail.com>
2023-07-26 10:41:24 +0530

    petroleEngine added

--- vehical.java
+++ vehical.java
@@ -1,9 +1,7 @@
 public class vehical {
 
-    private PetrolEngine pE;
-
     public static void main(String[] args) {
-        System.out.println("hello");
+        PetroleEngine pE = new PetroleEngine();
 
         pE.start();
     }
